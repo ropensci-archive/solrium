c6227cfc1ee609ea1bd1dea098eb18868959307d
schamberlain <myrmecocystus@gmail.com>
2013-12-30 16:30:50 -0800

    added startup file

---
+++ R/startup.r
@@ -0,0 +1,3 @@
+.onAttach <- function(...) {
+  packageStartupMessage("\n\nNew to solr? See the vignette at vignette('solr_vignette', 'solr')\nAnd vignette('solr_localsetup', 'solr') for tips on local setup of Solr\ncitation(package='solr') for the citation for this package \nUse suppressPackageStartupMessages() to suppress these startup messages in the future\n")
+}
