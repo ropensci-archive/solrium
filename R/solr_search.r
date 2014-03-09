5a42e5792abc43ef84e908c5e28d5f050b8b716c
Scott Chamberlain <myrmecocystus@gmail.com>
2014-03-09 09:41:18 -0700

    now solr_search with r like create vector instead of comma sep string, #22

--- R/solr_search.r
+++ R/solr_search.r
@@ -29,7 +29,7 @@
 #' 
 #' # Range searches
 #' ## Search for articles with Twitter count between 5 and 10
-#' solr_search(q='*:*', fl='alm_twitterCount,title', fq='alm_twitterCount:[5 TO 10]', 
+#' solr_search(q='*:*', fl=c('alm_twitterCount','title'), fq='alm_twitterCount:[5 TO 10]', 
 #' rows=3, url=url)
 #' 
 #' # Boosts
@@ -41,18 +41,18 @@
 #' 
 #' # Parse data, using the USGS BISON API
 #' url <- "http://bisonapi.usgs.ornl.gov/solr/occurrences/select"
-#' out <- solr_search(q='*:*', fl='scientific_name,latitude,longitude', url=url, raw=TRUE)
+#' out <- solr_search(q='*:*', fl=c('scientificName','decimalLatitude','decimalLongitude'), url=url, raw=TRUE)
 #' solr_parse(out, 'df')
 #' ## gives the same result
-#' solr_search(q='*:*', fl='scientific_name,latitude,longitude', url=url)
+#' solr_search(q='*:*', fl=c('scientificName','decimalLatitude','decimalLongitude'), url=url)
 #' 
 #' ## You can choose how to combine elements longer than length 1
-#' solr_search(q='*:*', fl='scientific_name,latitude,longitude', url=url, 
+#' solr_search(q='*:*', fl=c('scientificName','decimalLatitude','decimalLongitude'), url=url, 
 #'    parsetype='df', concat=';')
 #' 
 #' # Using the USGS BISON API (http://bison.usgs.ornl.gov/services.html#solr)
 #' ## the species names endpoint
-#' url2 <- "http://bisonapi.usgs.ornl.gov/solr/species/select"
+#' url2 <- "http://bisonapi.usgs.ornl.gov/solr/scientificName/select"
 #' solr_search(q='*:*', url=url2, parsetype='list')
 #' 
 #' # FunctionQuery queries
@@ -63,21 +63,21 @@
 #' ## metrics for articles in PLOS Journals
 #' url <- 'http://api.plos.org/search'
 #' solr_search(q="{!func}product($v1,$v2)", v1 = 'sqrt(counter_total_all)', 
-#'    v2 = 'log(alm_twitterCount)', rows=5, fl='id,title', fq='doc_type:full', 
+#'    v2 = 'log(alm_twitterCount)', rows=5, fl=c('id','title'), fq='doc_type:full', 
 #'    url=url) 
 #'    
 #' ## here, search on the product of counter_total_all and alm_twitterCount, using 
 #' ## a new temporary field "_val_"
 #' solr_search(q='_val_:"product(counter_total_all,alm_twitterCount)"', 
-#'    rows=5, fl='id,title', fq='doc_type:full', url=url) 
+#'    rows=5, fl=c('id','title'), fq='doc_type:full', url=url) 
 #' 
 #' ## papers with most citations   
 #' solr_search(q='_val_:"max(counter_total_all)"', 
-#'    rows=5, fl='id,counter_total_all', fq='doc_type:full', url=url) 
+#'    rows=5, fl=c('id','counter_total_all'), fq='doc_type:full', url=url) 
 #'    
 #' ## papers with most tweets
 #' solr_search(q='_val_:"max(alm_twitterCount)"', 
-#'    rows=5, fl='id,alm_twitterCount', fq='doc_type:full', url=url) 
+#'    rows=5, fl=c('id','alm_twitterCount'), fq='doc_type:full', url=url) 
 #' }
 
 solr_search <- function(q='*:*', sort=NULL, start=0, rows=NULL, pageDoc=NULL, 
@@ -89,10 +89,12 @@ solr_search <- function(q='*:*', sort=NULL, start=0, rows=NULL, pageDoc=NULL,
     stop("You must provide a url, e.g., http://api.plos.org/search or http://localhost:8983/solr/select")
   }
   
+  if(!is.null(fl)) names(fl) <- rep("fl", length(fl))
   args <- compact(list(q=q, sort=sort, start=start, rows=rows, pageDoc=pageDoc,
-                       pageScore=pageScore, fq=fq, fl=fl, defType=defType, 
-                       timeAllowed=timeAllowed, qt=qt, wt=wt, NOW=NOW, TZ=TZ,
-                       echoHandler=echoHandler, echoParams=echoParams))
+      pageScore=pageScore, fq=fq, defType=defType, 
+      timeAllowed=timeAllowed, qt=qt, wt=wt, NOW=NOW, TZ=TZ,
+      echoHandler=echoHandler, echoParams=echoParams))
+  args <- c(args, fl)
   
   # additional parameters
   args <- c(args, list(...))
