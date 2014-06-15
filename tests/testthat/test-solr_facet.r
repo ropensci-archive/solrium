85c367e0e70f9963774565f5bef5dcd89b080ea0
Scott Chamberlain <myrmecocystus@gmail.com>
2014-06-14 22:33:07 -0700

    fxied broken test

--- tests/testthat/test-solr_facet.r
+++ tests/testthat/test-solr_facet.r
@@ -7,9 +7,9 @@ a <- solr_facet(q='*:*', facet.field='journal', base=url, verbose=FALSE)
 b <- solr_facet(q='*:*', base=url, facet.date='publication_date', facet.date.start='NOW/DAY-5DAYS', facet.date.end='NOW', facet.date.gap='+1DAY', verbose=FALSE)
 
 test_that("solr_facet returns the correct dimensions in the data.frame", {
-  expect_that(length(a), equals(4))
-  expect_that(length(a$facet_queries), equals(0))
-  expect_that(nrow(a$facet_fields$journal), equals(10))
+  expect_equal(length(a), 4)
+  expect_equal(length(a$facet_queries), 0)
+  expect_equal(NCOL(a$facet_fields$journal), 2)
   
   expect_that(length(b), equals(4))
   expect_that(length(b$facet_dates), equals(1))
