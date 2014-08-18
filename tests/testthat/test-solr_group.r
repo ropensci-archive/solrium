e5038f8607860af81163eaa98e83f533c6334be0
Scott Chamberlain <myrmecocystus@gmail.com>
2014-08-18 10:43:51 -0700

    fixed tesets for solr_group and solr_stats fxns

--- tests/testthat/test-solr_group.r
+++ tests/testthat/test-solr_group.r
@@ -12,15 +12,15 @@ d <- solr_parse(out, 'df')
 e <- solr_group(q='ecology', group.field='journal', group.limit=3, fl=c('id','score'), 
                 group.format='grouped', group.main='true', base=url, verbose=FALSE)
 
-library(rjson)
+library('rjson')
 f <- fromJSON(out)
 
 test_that("solr_search returns the correct dimensions in the data.frame", {
-  expect_that(dim(a), equals(c(26,5)))
-  expect_that(dim(b), equals(c(26,6)))
+  expect_that(dim(a), equals(c(27,5)))
+  expect_that(dim(b), equals(c(27,6)))
   expect_that(length(c), equals(1))
   expect_that(length(d), equals(2))
-  expect_that(dim(d$article_type), equals(c(29,4)))
+  expect_that(dim(d$article_type), equals(c(27,4)))
   expect_that(dim(e), equals(c(10,4)))
   expect_that(length(f), equals(1))
   expect_that(length(f$grouped), equals(2))
